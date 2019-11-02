dd1f4e86338b9dbdfaddc082b02aabae9015f48a
Vipin Nair <swvist@gmail.com>
2019-11-02 13:43:43 -0700

    Project Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sat
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sat/core_test.clj
@@ -0,0 +1,7 @@
+(ns sat.core-test
+  (:require [clojure.test :refer :all]
+            [sat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
