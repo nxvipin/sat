5606ccd31ff6f6a9cca6e449d0d80d3f47363497
Vipin Nair <swvist@gmail.com>
2020-04-26 21:57:52 -0700

    Fns To Initalize CDCL Context Map

--- src/sat/cdcl.clj
+++ src/sat/cdcl.clj
@@ -51,3 +51,54 @@
                                   ::decision-level
                                   ::variables-assigned
                                   ::variables-unassigned]))
+
+(def test-input {:dimacs
+                 {:problem (list 3 5),
+                  :clauses (list [1 -5 4]
+                                 [-1 5 3 4]
+                                 [-3 -4])}})
+
+
+(defn literal->variable
+  "Given a clause literal, return the variable it represents"
+  [literal]
+  (max literal (- literal)))
+
+(defn input->clauses
+  "Given a dimacs input, return the list of clauses"
+  [input]
+  (-> input
+      :dimacs
+      :clauses))
+
+(defn init-context-variable-fields
+  "Given a context with `:clauses`, set context `:variables`,
+  `:variables-assigned` and `:variables-unassigned` and return the updated
+  context"
+  [context]
+  (let [variables (->> context
+                       :clauses
+                       flatten
+                       (map literal->variable)
+                       set)]
+    (-> context
+        (assoc :variables variables)
+        (assoc :variables-assigned {})
+        (assoc :variables-unassigned variables))))
+
+(defn init-context-watched-literals
+  "Given a context with clauses, set context watched literals and return updated
+  context"
+  [context]
+  (->> context
+       :clauses
+       (map #(hash-map %1 [(first %1) (second %1)]))
+       (assoc context :watched-literals)))
+
+(defn initialize-context
+  "Given a dimacs input, intialize and return the context map"
+  [input]
+  (-> {:clauses (input->clauses input)
+       :decision-level 0}
+      init-context-variable-fields
+      init-context-watched-literals))
